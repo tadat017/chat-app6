e3ce5f3d202905011d30cf31d0acc82cc61c91aa
tadat017 <tadat017@gmail.com>
2024-05-29 17:29:18 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   devise_for :users
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
